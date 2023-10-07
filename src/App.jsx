43578adc7468105bbf637453bc1a71a3d4cc7ab3
Adeolu Amole <adeoluamole@gmail.com>
2023-10-07 12:18:47 +0100

    Working on the project perfection

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,10 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap/dist/js/bootstrap.bundle";
 import axios from "axios";
-import Fetch from "./Fetch";
 import Jokes from "./Jokes";
-import Portal from "./Portal";
 import {
   Route,
   Routes,
@@ -17,7 +13,6 @@ import {
   Navigate,
 } from "react-router-dom";
 import ErrorPage from "./ErrorPage";
-import Card from "./Card";
 import Users from "./Users";
 import QuestionsAndAnswers from "./QuestionsAndAnswers";
 import DictionaryApp from "./DictionaryApp";
---
+++ src/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const ErrorPage = () => {
+  return (
+    <div className="error-container">
+      <div className="error-content">
+        <h1>Oops! Something went wrong.</h1>
+        <p>We apologize for the inconvenience. Please try again later.</p>
+      </div>
+    </div>
+  );
+};
+
+export default ErrorPage;
