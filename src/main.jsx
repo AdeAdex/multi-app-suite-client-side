07a2a0fa76d9734cb53fcbfe067f717e9dd523fd
Adeolu Amole <adeoluamole@gmail.com>
2023-10-08 15:32:16 +0100

    Working on the project perfection

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { MantineProvider } from '@mantine/core'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <MantineProvider>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  </MantineProvider>
 )
