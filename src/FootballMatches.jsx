5157dc058f5606bd7583edbb5e3dfdabcce86fc8
Adeolu Amole <adeoluamole@gmail.com>
2023-10-08 10:04:57 +0100

    Working on the project perfection

--- src/FootballMatches.jsx
+++ src/FootballMatches.jsx
@@ -5,20 +5,43 @@ import axios from 'axios';
 const FootballMatches = () => {
   const [matches, setMatches] = useState([]);
 
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       // const response = await axios.get('https://footbal-matches-adex2210.vercel.app/api/football-matches');
+  //       const response = await axios.get('/api/football-matches');
+
+  //       setMatches(response.data);
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // const response = await axios.get('https://footbal-matches-adex2210.vercel.app/api/football-matches');
-        const response = await axios.get('/api/football-matches');
-
-        setMatches(response.data);
+        const response = await axios.get('https://multi-app-suite-server-side.vercel.app/api/football-matches');
+        console.log('Response data:', response); // Debugging line
+  
+        // Ensure that response.data is an array before setting it in the state
+        if (Array.isArray(response.data)) {
+          setMatches(response.data);
+          console.log('Matches state after setting:', matches); // Debugging line
+        } else {
+          console.error('Data received is not an array:', response.data);
+        }
       } catch (error) {
         console.error('Error fetching data:', error);
       }
     };
-
+  
     fetchData();
   }, []);
+  
 
   return (
     <div>
