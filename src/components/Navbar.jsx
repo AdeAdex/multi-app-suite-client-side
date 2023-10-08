69cc0ef9a927f9d01e0d002ebccf2d314c57285f
Adeolu Amole <adeoluamole@gmail.com>
2023-10-08 15:12:12 +0100

    Working on the project perfection

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,104 +1,3 @@
-// import React, { useState } from "react";
-// import { Link } from "react-router-dom";
-
-// const Navbar = () => {
-//   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-//   const toggleMenu = () => {
-//     setIsMenuOpen(!isMenuOpen);
-//   };
-
-//   const closeMenu = () => {
-//     setIsMenuOpen(false);
-//   };
-
-//   return (
-//     <>
-//       <nav
-//         className={`navbar navbar-expand-lg bg-light ${
-//           isMenuOpen ? "show" : ""
-//         }`}
-//         style={{ position: "fixed", top: "0", left: "0", width: "100%" }}
-//       >
-//         <div className="container-fluid">
-//           <a className="navbar-brand" href="#">
-//             <img src="ade.png" alt="" style={{ width: "50px" }} />
-//           </a>
-//           <button
-//             className="navbar-toggler"
-//             type="button"
-//             data-bs-toggle="collapse"
-//             data-bs-target="#navbarSupportedContent"
-//             aria-controls="#navbarSupportedContent"
-//             aria-expanded="false"
-//             aria-label="Toggle navigation"
-//           >
-//             <span className="navbar-toggler-icon"></span>
-//           </button>
-//           <div
-//             className={`collapse navbar-collapse ${isMenuOpen ? "show" : ""}`}
-//             id="navbarSupportedContent"
-//           >
-//             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-//               <li className="nav-item">
-//                 <Link
-//                   className="nav-link active"
-//                   aria-current="page"
-//                   to="/"
-//                   onClick={closeMenu}
-//                 >
-//                   Home
-//                 </Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link to="/" className="nav-link" onClick={closeMenu}>
-//                   Jokes
-//                 </Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link to="/quiz" className="nav-link" onClick={closeMenu}>
-//                   Quiz
-//                 </Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link to="/dictionary" className="nav-link" onClick={closeMenu}>
-//                   Dictionary
-//                 </Link>
-//               </li>
-//               <li className="nav-item">
-//                 <Link
-//                   to="/football-matches"
-//                   className="nav-link"
-//                   onClick={closeMenu}
-//                 >
-//                   Football Matches
-//                 </Link>
-//               </li>
-//             </ul>
-//             <form className="d-flex" role="search">
-//               <input
-//                 className="form-control me-2"
-//                 type="search"
-//                 placeholder="Search"
-//                 aria-label="Search"
-//               />
-//               <button className="btn btn-outline-success" type="submit">
-//                 Search
-//               </button>
-//             </form>
-//           </div>
-//         </div>
-//       </nav>
-//     </>
-//   );
-// };
-
-// export default Navbar;
-
-
-
-
-
 
 import * as React from 'react';
 import PropTypes from 'prop-types';
@@ -116,7 +15,7 @@ import MenuIcon from '@mui/icons-material/Menu';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 
 const drawerWidth = 240;
 const navItems = ['Home', 'Jokes', 'Quiz', 'Dictionary', 'Football Matches'];
