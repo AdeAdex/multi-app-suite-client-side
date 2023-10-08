69e991d8fafd9dce25b71b9da2d983e0d8d71c33
Adeolu Amole <adeoluamole@gmail.com>
2023-10-08 08:03:10 +0100

    Working on the football match page

--- vite.config.js
+++ vite.config.js
@@ -1,37 +1,14 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
-
-
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import { VitePWA } from "vite-plugin-pwa";
-import tailwindcss from "tailwindcss";
-
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  build: {
-    rollupOptions: {
-      output: {
-        assetFileNames: "assets/[name].[ext]",
-        chunkFileNames: "assets/[name].[hash].js",
-        entryFileNames: "assets/[name].[hash].js",
-        manualChunks(id) {
-          if (id.includes("node_modules")) {
-            return "vendor";
-          }
-        },
-      },
-    },
-  },
+  plugins: [react()],
   server: {
-    port: 5173,
-    host: "0.0.0.0",
-  },
-});
+    port: 3000,
+    hostname: '0.0.0.0'
+  }
+})
+
 
