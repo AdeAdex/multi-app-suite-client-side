f83e4690e8236ee0b88de9e8921af18baa3cb6bf
Adeolu Amole <adeoluamole@gmail.com>
2023-10-10 07:35:56 +0100

    Working on the project perfection

--- src/FootballMatches.jsx
+++ src/FootballMatches.jsx
@@ -15,10 +15,9 @@
 //         console.error('Error fetching data:', error);
 //       }
 //     };
-  
+
 //     fetchData();
 //   }, []);
-  
 
 //   return (
 //     <div className="football-matches-container">
@@ -75,11 +74,8 @@
 
 // export default FootballMatches;
 
-
-
-
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 const FootballMatches = () => {
   const [matches, setMatches] = useState([]);
@@ -87,11 +83,13 @@ const FootballMatches = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get('https://multi-app-suite-server-side.vercel.app/api/football-matches');
-        console.log('Response data:', response.data.matches); 
-        setMatches(response.data.matches);
+        const response = await axios.get(
+          "http://localhost:3000/api/football-matches"
+        );
+        console.log("Response data:", response);
+        // setMatches(response.data.matches);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -101,24 +99,24 @@ const FootballMatches = () => {
   // Create a function to group matches by league name
   const groupMatchesByLeague = (matches) => {
     const groupedMatches = {
-      'Premier League': [],
-      'La Liga': [],
-      'Serie A': [],
-      'Bundesliga': [],
-      'Ligue 1': [],
-      'Eredivisie': [],
-      'Campeonato Brasileiro Série A': [],
-      'Primera Division': [],
-      'Primeira Liga': [],
-      'Others': [],
+      "Premier League": [],
+      "La Liga": [],
+      "Serie A": [],
+      Bundesliga: [],
+      "Ligue 1": [],
+      Eredivisie: [],
+      "Campeonato Brasileiro Série A": [],
+      "Primera Division": [],
+      "Primeira Liga": [],
+      Others: [],
     };
     matches.forEach((match) => {
-      const competitionName = match.competition?.name || 'Others';
-      const leagueCountryName = match.area || 'Unknown'; // Include area.name as league-country-name
+      const competitionName = match.competition?.name || "Others";
+      const leagueCountryName = match.area || "Unknown"; // Include area.name as league-country-name
       if (groupedMatches.hasOwnProperty(competitionName)) {
         groupedMatches[competitionName].push({ ...match, leagueCountryName });
       } else {
-        groupedMatches['Others'].push({ ...match, leagueCountryName });
+        groupedMatches["Others"].push({ ...match, leagueCountryName });
       }
     });
     return groupedMatches;
@@ -128,68 +126,99 @@ const FootballMatches = () => {
 
   return (
     <>
-    <div id="football-matches-page" className="">
-    <div className="football-matches-container">
-        <h1 className="matches-heading text-center">Football Matches Update</h1>
-        {Object.keys(groupedMatches).map((leagueName) => (
-          <div key={leagueName}>
-            <div className="league-info d-flex justify-content-between me-3">
-            <div className='d-flex gap-3'>
-            <img src={groupedMatches[leagueName][0]?.leagueCountryName.flag} alt="" style={{width: '30px'}} />
-              <div>
-                <h5 className="league-heading"> <strong>{leagueName}</strong></h5>
-                <small className='league-country-name'>{groupedMatches[leagueName][0]?.leagueCountryName.name}</small>
-              </div>
-            </div>
-              <img className="league-flag" src={groupedMatches[leagueName][0]?.competition?.emblem} alt={leagueName} />
-            </div>
-            <ul className="matches-list">
-              {groupedMatches[leagueName].map((match) => (
-                <li className="match-item" key={match.id}>
-                  <div className="match-details">
-                    <div className="team-info">
-                      {match.homeTeam && (
-                        <>
-                          <img className="team-crest" src={match.homeTeam.crest} alt={match.homeTeam.name} />
-                          <span className="team-name">{match.homeTeam.name}</span>
-                        </>
-                      )}
-                    </div>
-                    <div className="score">
-                      {match.score && (
-                        <>
-                          {match.score.winner === 'HOME_TEAM' && <span className="winner-text">(Winner)</span>}
-                          {match.score.fullTime && (
-                            <>
-                              {match.score.fullTime.home} - {match.score.fullTime.away}
-                            </>
-                          )}
-                        </>
-                      )}
-                    </div>
-                    <div className="team-info">
-                      {match.awayTeam && (
-                        <>
-                          <img className="team-crest" src={match.awayTeam.crest} alt={match.awayTeam.name} />
-                          <span className="team-name">{match.awayTeam.name}</span>
-                        </>
-                      )}
-                    </div>
+      <div id="football-matches-page" className="">
+        <div className="football-matches-container">
+          <h1 className="matches-heading text-center">
+            Football Matches Update
+          </h1>
+          {Object.keys(groupedMatches).map((leagueName) => (
+            <div key={leagueName}>
+              <div className="league-info d-flex justify-content-between me-3">
+                <div className="d-flex gap-3">
+                  <img
+                    src={groupedMatches[leagueName][0]?.leagueCountryName.flag}
+                    alt=""
+                    style={{ width: "30px" }}
+                  />
+                  <div>
+                    <h5 className="league-heading">
+                      {" "}
+                      <strong>{leagueName}</strong>
+                    </h5>
+                    <small className="league-country-name">
+                      {groupedMatches[leagueName][0]?.leagueCountryName.name}
+                    </small>
                   </div>
-                  <div className="match-info">
-  {match.status !== 'FINISHED' && (
-    <p className="match-date">Match starts on {new Date(match.utcDate).toLocaleString()}</p>
-  )}
-</div>
-
-
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+                </div>
+                <img
+                  className="league-flag"
+                  src={groupedMatches[leagueName][0]?.competition?.emblem}
+                  alt={leagueName}
+                />
+              </div>
+              <ul className="matches-list">
+                {groupedMatches[leagueName].map((match) => (
+                  <li className="match-item" key={match.id}>
+                    <div className="match-details">
+                      <div className="team-info">
+                        {match.homeTeam && (
+                          <>
+                            <img
+                              className="team-crest"
+                              src={match.homeTeam.crest}
+                              alt={match.homeTeam.name}
+                            />
+                            <span className="team-name">
+                              {match.homeTeam.name}
+                            </span>
+                          </>
+                        )}
+                      </div>
+                      <div className="score">
+                        {match.score && (
+                          <>
+                            {match.score.winner === "HOME_TEAM" && (
+                              <span className="winner-text">(Winner)</span>
+                            )}
+                            {match.score.fullTime && (
+                              <>
+                                {match.score.fullTime.home} -{" "}
+                                {match.score.fullTime.away}
+                              </>
+                            )}
+                          </>
+                        )}
+                      </div>
+                      <div className="team-info">
+                        {match.awayTeam && (
+                          <>
+                            <img
+                              className="team-crest"
+                              src={match.awayTeam.crest}
+                              alt={match.awayTeam.name}
+                            />
+                            <span className="team-name">
+                              {match.awayTeam.name}
+                            </span>
+                          </>
+                        )}
+                      </div>
+                    </div>
+                    <div className="match-info">
+                      {match.status !== "FINISHED" && (
+                        <p className="match-date">
+                          Match starts on{" "}
+                          {new Date(match.utcDate).toLocaleString()}
+                        </p>
+                      )}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
     </>
   );
 };
